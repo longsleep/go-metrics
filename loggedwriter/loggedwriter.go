476a419840f1c7a9fa09d6cac8d93c626519d67c
Simon Eisenmann <simon@longsleep.org>
2021-03-22 18:24:37 +0100

    Support http.Flusher and http.Pusher

--- loggedwriter/loggedwriter.go
+++ loggedwriter/loggedwriter.go
@@ -1,4 +1,4 @@
-package metrics
+package loggedwriter
 
 import (
 	"net/http"
@@ -23,13 +23,98 @@ type loggedResponseWriterHijacker struct {
 	http.Hijacker
 }
 
+// loggedResponseWriterHijacker is a http.ResponseWriter and http.Flusher
+// with Status.
+type loggedResponseWriterFlusher struct {
+	LoggedResponseWriter
+	http.Flusher
+}
+
+// loggedResponseWriterHijacker is a http.ResponseWriter and http.Pusher
+// with Status.
+type loggedResponseWriterPusher struct {
+	LoggedResponseWriter
+	http.Pusher
+}
+
+// loggedResponseWriterHijackerFlusherPusher is a http.ResponseWriter, http.Hijacker,
+// http.Flusher and http.Pusher with Status.
+type loggedResponseWriterHijackerFlusherPusher struct {
+	LoggedResponseWriter
+	http.Hijacker
+	http.Flusher
+	http.Pusher
+}
+
+// loggedResponseWriterHijackerFlusher is a http.ResponseWriter, http.Hijacker,
+// and http.Flusher with Status.
+type loggedResponseWriterHijackerFlusher struct {
+	LoggedResponseWriter
+	http.Hijacker
+	http.Flusher
+}
+
+// loggedResponseWriterHijackerPusher is a http.ResponseWriter, http.Hijacker,
+// and http.Pusher with Status.
+type loggedResponseWriterHijackerPusher struct {
+	LoggedResponseWriter
+	http.Hijacker
+	http.Pusher
+}
+
+// loggedResponseWriterFlusherPusher is a http.ResponseWriter, http.Flusher and
+// http.Pusher with Status.
+type loggedResponseWriterFlusherPusher struct {
+	LoggedResponseWriter
+	http.Flusher
+	http.Pusher
+}
+
 // NewLoggedResponseWriter wraps the provided http.ResponseWriter with Status
 // preserving the support to hijack the connection if supported by the provided
 // http.ResponseWriter.
 func NewLoggedResponseWriter(w http.ResponseWriter) LoggedResponseWriter {
 	lw := &loggedResponseWriter{ResponseWriter: w}
-	if hj, ok := w.(http.Hijacker); ok {
+
+	hj, hj_ok := w.(http.Hijacker)
+	fl, fl_ok := w.(http.Flusher)
+	ps, ps_ok := w.(http.Pusher)
+
+	if hj_ok {
+		if fl_ok && ps_ok {
+			return &loggedResponseWriterHijackerFlusherPusher{
+				LoggedResponseWriter: lw,
+				Hijacker:             hj,
+				Flusher:              fl,
+				Pusher:               ps,
+			}
+		}
+		if fl_ok {
+			return &loggedResponseWriterHijackerFlusher{
+				LoggedResponseWriter: lw,
+				Hijacker:             hj,
+				Flusher:              fl,
+			}
+		}
+		if ps_ok {
+			return &loggedResponseWriterHijackerPusher{
+				LoggedResponseWriter: lw,
+				Hijacker:             hj,
+				Pusher:               ps,
+			}
+		}
 		return &loggedResponseWriterHijacker{LoggedResponseWriter: lw, Hijacker: hj}
+	} else if fl_ok {
+		if ps_ok {
+			return &loggedResponseWriterFlusherPusher{
+				LoggedResponseWriter: lw,
+				Flusher:              fl,
+				Pusher:               ps,
+			}
+		}
+		return &loggedResponseWriterFlusher{LoggedResponseWriter: lw, Flusher: fl}
+	} else if ps_ok {
+		return &loggedResponseWriterPusher{LoggedResponseWriter: lw, Pusher: ps}
 	}
 
 	return lw
@@ -41,7 +126,7 @@ func (w *loggedResponseWriter) WriteHeader(status int) {
 	w.ResponseWriter.WriteHeader(status)
 }
 
-// Status returns the written HTTP respons header status code or http.StatusOK
+// Status returns the written HTTP response header status code or http.StatusOK
 // if none was written.
 func (w *loggedResponseWriter) Status() int {
 	status := w.status
